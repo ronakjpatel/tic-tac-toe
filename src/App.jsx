811b1a252e3d040c10837eb497a612bf782cd2d5
ronakjpatel <rjpatel7991@gmail.com>
2024-05-23 13:07:22 -0400

    setup the basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 function App() {
   return (
-    <></>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">PLAYER 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">PLAYER 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
   );
 }
 
